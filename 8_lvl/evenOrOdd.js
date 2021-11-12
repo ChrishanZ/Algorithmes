47eb7330ce81ad02733af834884b996db3cccefb
Chrishan.Duttoo <chrisduttoo@gmail.com>
2021-11-12 16:34:56 +0100

    Level 8

---
+++ 8_lvl/evenOrOdd.js
@@ -0,0 +1,15 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// const even_or_odd = (number) => {
+//   if (number % 2 === 0) {
+//     return "Even";
+//   } else {
+//     return "Odd";
+//   }
+// };
+
+const even_or_odd = (number) => {
+  number % 2 ? "Odd" : "Even";
+};
+
+even_or_odd(7);
